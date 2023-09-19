4063cb2944f4ca6969f21ca02f3bdeeecffe0576
Crystian <crystian.lf@gmail.com>
2023-09-19 19:45:50 -0300

    Criando funcao de conversao

--- conversao-jpg-png.sh
+++ conversao-jpg-png.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-CAMINHO_IMAGEM=~/Downloads/imagens-livros
+converte-imagem(){
 
-for imagem in $@
+cd ~/Downloads/imagens-livros
+
+if [! -d png] 
+then
+    mkdir png
+fi
+
+for imagem in *.jpg
 do
-    convert $CAMINHO_IMAGEM/$imagem.jpg $CAMINHO_IMAGEM/$imagem.png
+    local img_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+    convert $imagem.jpg png/$imagem.png
 done
+
+}
+
+converte-imagem 2>erros_conversao.txt
+
+if [ $? -eq 0 ] then
+    echo "Conversao realizada com sucesso"
+else
+    echo "Ocorreu um erro na conversao"
+
+fi
